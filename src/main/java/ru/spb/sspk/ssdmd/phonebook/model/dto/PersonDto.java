62e0db75a657f99f6dd7f10c4320b5e43b792dd9
AntiStC <94138193+AntiStC@users.noreply.github.com>
2022-08-26 16:48:24 +0300

    create person

---
+++ src/main/java/ru/spb/sspk/ssdmd/phonebook/model/dto/PersonDto.java
@@ -0,0 +1,126 @@
+package ru.spb.sspk.ssdmd.phonebook.model.dto;
+
+public class PersonDto {
+
+    private Long id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String department;
+    private Integer phone;
+
+    public PersonDto() {
+    }
+
+    public static class Builder{
+        private Long id;
+        private String firstName;
+        private String middleName;
+        private String lastName;
+        private String department;
+        private Integer phone;
+
+        public Builder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setMiddleName(String middleName) {
+            this.middleName = middleName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setDepartment(String department) {
+            this.department = department;
+            return this;
+        }
+
+        public Builder setPhone(Integer phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public PersonDto build(){
+            return new PersonDto(this);
+        }
+    }
+
+    public PersonDto(Builder builder) {
+        this.id = builder.id;
+        this.firstName = builder.firstName;
+        this.middleName = builder.middleName;
+        this.lastName = builder.lastName;
+        this.department = builder.department;
+        this.phone = builder.phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonDto{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", department='" + department + '\'' +
+                ", phone=" + phone +
+                '}';
+    }
+}
---
+++ src/main/java/ru/spb/sspk/ssdmd/phonebook/model/entity/Person.java
@@ -0,0 +1,174 @@
+package ru.spb.sspk.ssdmd.phonebook.model.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Data
+@Table(name = "person",catalog = "public",schema = "sspk_ssdmd_pb")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "first_name")
+    private String firstName;
+
+    @NotNull
+    @Column(name = "middle_name")
+    private String middleName;
+
+    @NotNull
+    @Column(name = "last_name")
+    private String lastName;
+
+    @NotNull
+    @Column(name = "department")
+    private String department;
+
+    @NotNull
+    @Column(name = "phone")
+    private Integer phone;
+
+    public Person() {
+    }
+    public static class Builder{
+        private Long id;
+        private String firstName;
+        private String middleName;
+        private String lastName;
+        private String department;
+        private Integer phone;
+
+        public Builder setId(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder setMiddleName(String middleName) {
+            this.middleName = middleName;
+            return this;
+        }
+
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setDepartment(String department) {
+            this.department = department;
+            return this;
+        }
+
+        public Builder setPhone(Integer phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Person build(){
+            return new Person(this);
+        }
+    }
+
+    public Person(Builder builder) {
+        this.id = builder.id;
+        this.firstName = builder.firstName;
+        this.middleName = builder.middleName;
+        this.lastName = builder.lastName;
+        this.department = builder.department;
+        this.phone = builder.phone;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+
+        Person person = (Person) o;
+
+        if (!firstName.equals(person.firstName)) return false;
+        if (!middleName.equals(person.middleName)) return false;
+        if (!lastName.equals(person.lastName)) return false;
+        if (!department.equals(person.department)) return false;
+        return phone.equals(person.phone);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName.hashCode();
+        result = 31 * result + middleName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + department.hashCode();
+        result = 31 * result + phone.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", department='" + department + '\'' +
+                ", phone=" + phone +
+                '}';
+    }
+}
